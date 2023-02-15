082491e34ba959666216056173c9e61a04f71392
Habeebllah <lasisihabeeb67@gmail.com>
2023-02-15 14:33:36 +0100

    Updates

---
+++ feb13/index.js
@@ -0,0 +1,29 @@
+let x, address, names, present, absent
+x = 4
+address = "It's Idi Aba, Abeokuta Ogun State"
+names = 'Habeeb'
+present = true
+absent = false
+let age = 5
+
+const product = null
+console.log(age*6)
+console.log(x*2)
+
+let user_input = prompt("Enter your age:  ")
+console.log("In the next 5 years your will be", parseInt(user_input)+5, "old")
+
+let w, z
+w = 5
+z = 8
+// Arithmetic Operators
+console.log(w+z)
+console.log(w*z)
+console.log(w/z)
+console.log(z%w)
+console.log(w**z)
+console.log(w-z)
+w *= 5
+console.log(w)
+
+//Comparison or Boolean Operators
